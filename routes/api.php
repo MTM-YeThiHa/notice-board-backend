112d1eb8eff15b19524bc101412f565d1aa37869
ZawHtetAungSCM <zawhtetaung@metateammyanmar.com>
2023-11-08 13:21:43 +0630

    Remove Package-Lock

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return 'OK';
+});
+
 Route::prefix('auth')->group(function () {
     Route::get('/check-token', [UserController::class, 'checkToken']);
     Route::post('/login', [UserController::class, 'login']);
